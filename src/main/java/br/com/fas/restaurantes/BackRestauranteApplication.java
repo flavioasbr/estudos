7df65020a0fe37c09ee3be3dc723a1f9ada672c5
Flavio A.Silva <flavioasbr@fas.com.br>
2022-08-09 17:35:00 -0300

    Inicio de tudo

---
+++ src/main/java/br/com/fas/restaurantes/BackRestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.fas.restaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fas/restaurantes/BackRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fas.restaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
